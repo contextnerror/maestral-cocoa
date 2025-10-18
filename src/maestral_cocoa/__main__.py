2d856c0cb3756ba7f23d20a764853be326cef426
Sam Schott <sam.schott@outlook.com>
2025-10-18 16:04:18 +0200

    Use freeze support from maestral cli

--- src/maestral_cocoa/__main__.py
+++ src/maestral_cocoa/__main__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import argparse
 
 from maestral.daemon import freeze_support as freeze_support_daemon
+from maestral.cli import freeze_support as freeze_support_cli
 
 
 def main():
@@ -19,22 +19,6 @@ def main():
 
     run(parsed_args.config_name)
 
-
-def freeze_support_cli():
-    """
-    Provides support to start the CLI from a frozen executable.
-    """
-
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("--cli", action="store_true")
-    parsed_args, remaining = parser.parse_known_args()
-
-    if parsed_args.cli:
-        from maestral.cli import main
-
-        sys.argv = ["maestral"] + remaining
-        main(prog_name="maestral")
-        sys.exit()
 
 
 if __name__ == "__main__":
